fac822900e259e0cd5f04ded7f0c0bd3a66ac9be
Argishti <argishti.yeghiazaryan@betconstruct.com>
2018-07-04 03:27:06 +0400

    search functionality results injected to itemsWrapper component

--- src/components/itemsWrapper.js
+++ src/components/itemsWrapper.js
@@ -1,9 +1,11 @@
 import React, { Component, PureComponent } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class SingleItem extends PureComponent {
 
     render () {
-        return <p>{"single item"}</p>
+        return <p>{this.props.data}</p>
     }
 }
 
@@ -13,10 +15,21 @@ class ItemsWrapper extends Component {
         super(props);
     }
     render () {
-        return (
-            <SingleItem />
-        )
+        console.log(this.props);
+        return this.props.loadingState ? Object.keys(this.props.videoData).map((val, key) =>
+            <SingleItem data = {val} key = {val} />
+        ) : null
     }
 }
-export default ItemsWrapper;
+const mapStateToProps = (state) => {
+    return {
+        videoData: state.videoSearchReducer && state.videoSearchReducer.searchResult,
+        loadingState: state.loadingState && state.loadingState.searchResult && state.loadingState.searchResult.loaded
+    }
+};
+export default connect (mapStateToProps) (ItemsWrapper);
+
+ItemsWrapper.propTypes = {
+    videoData: PropTypes.object
+};
 
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import { SearchVideos } from '../actions/actions';
 import { connect } from 'react-redux';
+
 class SearchBar extends Component {
     constructor (props) {
         super(props);
-        this.props.dispatch(SearchVideos("searchVideos"))
+        this.handleVideoSearch = this.handleVideoSearch.bind(this);
+        this.state = {
+            searchKey: ""
+        }
     }
-    render () {
-        return (
-            <div>
-                <input type="text"/>
-            </div>
-        )
+    handleVideoSearch () {
+        return (e) => {
+            this.setState({searchKey: e.target.value.trim()});
+            if (this.searchVideos) {
+                clearTimeout(this.searchVideos)
+            }
+            this.searchVideos = setTimeout(() => this.props.dispatch(SearchVideos("searchResult", this.state.searchKey)), 400);
+
+        }
+    }
+
+        render () {
+            return (
+                <div>
+                    <input type="text" onChange={this.handleVideoSearch()}/>
+                </div>
+            )
     }
 }
 export default connect()(SearchBar);
